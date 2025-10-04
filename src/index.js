d9d3d3c10328a6c09521130a0ee0189707f547ca
[dyad] <Sheaglass91@gmail.com>
2025-10-04 19:23:36 +0100

    Reverted all changes back to version e6f1edde31c008d297875d85fe9154a1d3192308

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { useSnapshot } from 'valtio'
 import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom' // Import routing components
 import AboutPage from './pages/AboutPage' // Import new pages
 import ContactPage from './pages/ContactPage'
-import ShinyText from './components/ShinyText'; // Import the new ShinyText component
 
 // A layout component to keep the MainOverlay consistent across routes
 function Layout() {
@@ -15,17 +14,8 @@ function Layout() {
     <>
       <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
         {/* Central Title */}
-        <div style={{ position: 'absolute', top: 20, width: '100%', textAlign: 'center', pointerEvents: 'none', lineHeight: '1em' }}>
-          <ShinyText
-            speedInMs={3000}
-            style={{
-              fontSize: '4vw',
-              fontWeight: 'bold',
-              fontFamily: 'Helvetica, sans-serif',
-            }}
-          >
-            Shea.Glass
-          </ShinyText>
+        <div style={{ position: 'absolute', top: 20, width: '100%', textAlign: 'center', fontSize: '4vw', fontWeight: 'bold', color: 'white', pointerEvents: 'none', lineHeight: '1em' }}>
+          Shea.Glass
         </div>
         {/* Recent Projects Section */}
         <div style={{ position: 'absolute', top: 'calc(20px + 4vw + 10px)', width: '100%', textAlign: 'center', fontSize: '2vw', color: 'white', pointerEvents: 'none' }}>
